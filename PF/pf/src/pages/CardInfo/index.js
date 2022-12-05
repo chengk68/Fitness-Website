4d1bf82cfc94fb741c601f91c2b53b7be2ce798f
Qinyu2000 <wuqinyu2000@gmail.com>
2022-12-05 17:14:17 -0500

    Add cardInfo and payment

--- PF/pf/src/pages/CardInfo/index.js
+++ PF/pf/src/pages/CardInfo/index.js
@@ -1,9 +1,138 @@
-import React from 'react'
+import { Button, Grid, Paper, TextField } from "@mui/material";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function CardInfo() {
+  const [cardNumber, setCardNumber] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [postcode, setPostcode] = useState("");
+  const [editable, setEditable] = useState(false);
+  const [showCardInfo, setShowCardInfo] = useState(false);
+  const pstyle = {
+    padding: 20,
+    height: "60vh",
+    width: 400,
+    margin: "20px auto",
+  };
+  const config = {
+    headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+  };
+  const cardInfoURL = "http://localhost:8000/subscriptions/cardInfo/";
+  useEffect(() => {
+    axios
+      .get(cardInfoURL, config)
+      .then((result) => {
+        setCardNumber(result.data.cardNumber);
+        setExpirationDate(result.data.expirationDate);
+        setCvv(result.data.cvv);
+        setPostcode(result.data.postcode);
+        setShowCardInfo(true);
+      })
+      .catch((e) => {
+        console.log(e);
+        setShowCardInfo(false);
+      });
+  }, []);
+
+  const data = {
+    cardNumber,
+    expirationDate,
+    cvv,
+    postcode,
+  };
+  const submitChange = () => {
+    axios.put(cardInfoURL, data, config).catch((e) => {
+      console.log(e);
+    });
+  };
+  const tstyle = { margin: "10px 0" };
   return (
-	<div>CardInfo</div>
-  )
+    <Grid>
+      {showCardInfo ? (
+        <Paper elevation={10} style={pstyle}>
+          <TextField
+            label="Card number"
+            placeholder="Enter card number"
+            fullWidth
+            style={tstyle}
+            variant="standard"
+            disabled={!editable}
+            value={cardNumber}
+            onChange={(e) => {
+              setCardNumber(e.target.value);
+            }}
+          />
+          <TextField
+            label="Expiration date"
+            placeholder="xxxx-xx-xx"
+            fullWidth
+            variant="standard"
+            disabled={!editable}
+            style={tstyle}
+            value={expirationDate}
+            onChange={(e) => {
+              setExpirationDate(e.target.value);
+            }}
+          />
+          <TextField
+            label="cvv"
+            placeholder="Enter cvv (maximum 5 characters)"
+            fullWidth
+            variant="standard"
+            disabled={!editable}
+            style={tstyle}
+            value={cvv}
+            onChange={(e) => {
+              setCvv(e.target.value);
+            }}
+          />
+          <TextField
+            label="postcode"
+            placeholder="Enter postcode"
+            fullWidth
+            variant="standard"
+            disabled={!editable}
+            style={tstyle}
+            value={postcode}
+            onChange={(e) => {
+              setPostcode(e.target.value);
+            }}
+          />
+          {!editable ? (
+            <Button
+              fullWidth
+              variant="contained"
+              style={tstyle}
+              onClick={(e) => {
+                e.preventDefault();
+                setEditable(true);
+              }}
+            >
+              Edit
+            </Button>
+          ) : (
+            <Button
+              fullWidth
+              variant="contained"
+              style={tstyle}
+              onClick={(e) => {
+                e.preventDefault();
+                setEditable(false);
+                submitChange();
+              }}
+            >
+              Save
+            </Button>
+          )}
+        </Paper>
+      ) : (
+        <Paper elevation={10} style={pstyle}>
+          <p>There are no card information. Please subscribe first</p>
+        </Paper>
+      )}
+    </Grid>
+  );
 }
 
-export default CardInfo
+export default CardInfo;
--- PF/pf/src/pages/Payment/index.js
+++ PF/pf/src/pages/Payment/index.js
@@ -1,11 +1,112 @@
-import React from 'react'
-import { useParams } from 'react-router-dom/dist'
+import { Button, Grid, Paper, TextField, Alert } from "@mui/material";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom/dist";
 
 function Payment() {
-  const {id} = useParams()
+  const { id } = useParams();
+  const [cardNumber, setCardNumber] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [postcode, setPostcode] = useState("");
+  const [alert, setAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState("");
+  const navigate = useNavigate();
+  const subscribeURL = "http://localhost:8000/subscriptions/subscribe/";
+  const config = {
+    headers: { Authorization: "Bearer " + localStorage.getItem("token") },
+  };
+  const data = {
+    subscription_id: id,
+    cardNumber,
+    expirationDate,
+    cvv,
+    postcode,
+  };
+
+  const handleSubmit = () => {
+    axios
+      .post(subscribeURL, data, config)
+      .then((result) => {
+        console.log(result);
+        navigate("/subscription");
+      })
+      .catch((e) => {
+        console.log(e);
+        setAlert(true);
+        setAlertContent("Something goes wrong");
+      });
+  };
+  const pstyle = {
+    padding: 20,
+    height: "60vh",
+    width: 400,
+    margin: "20px auto",
+  };
+  const tstyle = { margin: "10px 0" };
   return (
-	<div>Payment {id}</div>
-  )
+    <Grid>
+      <Paper elevation={10} style={pstyle}>
+        <TextField
+          label="Card number"
+          placeholder="Enter card number"
+          fullWidth
+          required
+          style={tstyle}
+          value={cardNumber}
+          onChange={(e) => {
+            setCardNumber(e.target.value);
+          }}
+        />
+        <TextField
+          label="Expiration date"
+          placeholder="xxxx-xx-xx"
+          fullWidth
+          required
+          style={tstyle}
+          value={expirationDate}
+          onChange={(e) => {
+            setExpirationDate(e.target.value);
+          }}
+        />
+        <TextField
+          label="cvv"
+          placeholder="Enter cvv (maximum 5 characters)"
+          fullWidth
+          required
+          style={tstyle}
+          value={cvv}
+          onChange={(e) => {
+            setCvv(e.target.value);
+          }}
+        />
+        <TextField
+          label="postcode"
+          placeholder="Enter postcode"
+          fullWidth
+          required
+          style={tstyle}
+          value={postcode}
+          onChange={(e) => {
+            setPostcode(e.target.value);
+          }}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          style={{ margin: "10px 0", height: "60px" }}
+          onClick={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          Subscribe
+        </Button>
+        {alert ? <Alert severity="error">{alertContent}</Alert> : <></>}
+      </Paper>
+    </Grid>
+  );
 }
 
-export default Payment
+export default Payment;
