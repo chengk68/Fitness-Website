2886d03d1e39cbd5191ccd13e7483ca56571731a
Qinyu2000 <wuqinyu2000@gmail.com>
2022-12-07 22:14:14 -0500

    add logout button

--- PF/pf/src/components/Layout/index.js
+++ PF/pf/src/components/Layout/index.js
@@ -1,8 +1,10 @@
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material";
 import React from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
 
 function Layout() {
+	const navigate = useNavigate()
+
   return (
     <>
 	  <AppBar position="static">
@@ -25,6 +27,11 @@ function Layout() {
 			<IconButton color="inherit" size="small" sx={{width: "120px", height: "70px"}}>
 				<Link to="/MyHistory" style={{'textDecoration': 'none', color: "white"}}>MyHistory</Link>
 			</IconButton>
+			<IconButton color="inherit" size="small" sx={{width: "80px", height: "70px"}} onClick={(e) => {
+				e.preventDefault()
+				localStorage.clear()
+				navigate("/")
+			}}>Log Out</IconButton>
 		</Toolbar>
 	  </AppBar>
 	  <Outlet />
